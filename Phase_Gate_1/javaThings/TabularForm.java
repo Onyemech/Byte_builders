653d5d5c5b9f091f0706b3bd4142f908dded4f8a
Onyemechi Caleb <Onyemechicaleb4@gmail.com>
2024-09-30 16:30:38 +0100

    Could't meet up

---
+++ Phase_Gate_1/javaThings/TabularForm.java
@@ -0,0 +1,14 @@
+public class TabularForm {
+
+	public static void main (String[] args) {
+
+		System.out.println("a     b     a**b");
+
+		int counter = 0;
+
+		for (int count = 1; count < 6;count++) {
+			counter = count + 1;
+			System.out.println(count+"     "+counter+"    ");
+		}
+	}
+}
