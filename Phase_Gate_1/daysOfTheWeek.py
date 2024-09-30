50426238e912bc654d3b6df6be78ce3c3f87b68c
Onyemechi Caleb <Onyemechicaleb4@gmail.com>
2024-09-30 16:09:00 +0100

    Made some changes

--- Phase_Gate_1/daysOfTheWeek.py
+++ Phase_Gate_1/daysOfTheWeek.py
@@ -1,23 +1,120 @@
 
-day0 = 0
-day1 = "Monday"
-day2 = "Tuesday"
-day3 = "Wednesday"
-day4 = "Thursday"
-day5 = "Friday"
-day6 = "Saturday"
-
-
 user_input = int(input("Enter today's date "))
 user_input_2 = int(input("Enter the number of days elapsed since today:  "))
 
 match user_input:
-	case 0: print("Today is Sunday and the future day is ")
-		day5 = user_input += user_input_2
-	case 1: print("Today is Monday and the future day is ")
-	case 2: print("Today is Tuesday and the future day is ")
-	case 3: print("Today is Wednesday and the future day is ")
-	case 4: print("Today is Thursday and the future day is ")
-	case 5: print("Today is Friday and the future day is ")
-	case 6: print("Today is Saturday and the future day is ")
+	case 0: 
+		if user_input_2 == 1:
+			print("Today is Sunday and the future day is Monday")
+		elif user_input_2 == 2:
+			print("Today is Sunday and the future day is Tuesday")
+		elif user_input_2 == 3:
+			print("Today is Sunday and the future day is Wednesday")
+		elif user_input_2 == 4:
+			print("Today is Sunday and the future day is Thursday")
+		elif user_input_2 == 5:
+			print("Today is Sunday and the future day is Friday")
+		elif user_input_2 == 6:
+			print("Today is Sunday and the future day is Saturday")
+		elif user_input_2 == 0:
+			print("Today is Sunday and the future day is Sunday")
+
+	case 1:
+		if user_input_2 == 1:
+			print("Today is Monday and the future day is Monday")
+		elif user_input_2 == 2:
+			print("Today is Monday and the future day is Tuesday")
+		elif user_input_2 == 3:
+			print("Today is Monday and the future day is Wednesday")
+		elif user_input_2 == 4:
+			print("Today is Monday and the future day is Thursday")
+		elif user_input_2 == 5:
+			print("Today is Monday and the future day is Friday")
+		elif user_input_2 == 6:
+			print("Today is Monday and the future day is Saturday")
+		elif user_input_2 == 0:
+			print("Today is Monday and the future day is Sunday")
+
 		
+	case 2: 
+		if user_input_2 == 1:
+			print("Today is Tuesday and the future day is Monday")
+		elif user_input_2 == 2:
+			print("Today is Tuesday and the future day is Tuesday")
+		elif user_input_2 == 3:
+			print("Today is Tuesday and the future day is Wednesday")
+		elif user_input_2 == 4:
+			print("Today is Tuesday and the future day is Thursday")
+		elif user_input_2 == 5:
+			print("Today is Tuesday and the future day is Friday")
+		elif user_input_2 == 6:
+			print("Today is Tuesday and the future day is Saturday")
+		elif user_input_2 == 0:
+			print("Today is Tuesday and the future day is Sunday")
+
+		
+	case 3: 
+		if user_input_2 == 1:
+			print("Today is Wednesday and the future day is Monday")
+		elif user_input_2 == 2:
+			print("Today is Wednesday and the future day is Tuesday")
+		elif user_input_2 == 3:
+			print("Today is Wednesday and the future day is Wednesday")
+		elif user_input_2 == 4:
+			print("Today is Wednesday and the future day is Thursday")
+		elif user_input_2 == 5:
+			print("Today is Wednesday and the future day is Friday")
+		elif user_input_2 == 6:
+			print("Today is Wednesday and the future day is Saturday")
+		elif user_input_2 == 0:
+			print("Today is Wednesday and the future day is Sunday")
+
+	case 4:
+		if user_input_2 == 1:
+			print("Today is Thursday and the future day is Monday")
+		elif user_input_2 == 2:
+			print("Today is Thursday and the future day is Tuesday")
+		elif user_input_2 == 3:
+			print("Today is Thursday and the future day is Wednesday")
+		elif user_input_2 == 4:
+			print("Today is Thursday and the future day is Thursday")
+		elif user_input_2 == 5:
+			print("Today is Thursday and the future day is Friday")
+		elif user_input_2 == 6:
+			print("Today is Thursday and the future day is Saturday")
+		elif user_input_2 == 0:
+			print("Today is Thursday and the future day is Sunday")
+
+	case 5: 
+		if user_input_2 == 1:
+			print("Today is Friday and the future day is Monday")
+		elif user_input_2 == 2:
+			print("Today is Friday and the future day is Tuesday")
+		elif user_input_2 == 3:
+			print("Today is Friday and the future day is Wednesday")
+		elif user_input_2 == 4:
+			print("Today is Friday and the future day is Thursday")
+		elif user_input_2 == 5:
+			print("Today is Friday and the future day is Friday")
+		elif user_input_2 == 6:
+			print("Today is Friday and the future day is Saturday")
+		elif user_input_2 == 0:
+			print("Today is Friday and the future day is Sunday")
+
+	case 6:
+		if user_input_2 == 1:
+			print("Today is Saturday and the future day is Monday")
+		elif user_input_2 == 2:
+			print("Today is Saturday and the future day is Tuesday")
+		elif user_input_2 == 3:
+			print("Today is Saturday and the future day is Wednesday")
+		elif user_input_2 == 4:
+			print("Today is Saturday and the future day is Thursday")
+		elif user_input_2 == 5:
+			print("Today is Saturday and the future day is Friday")
+		elif user_input_2 == 6:
+			print("Today is Saturday and the future day is Saturday")
+		elif user_input_2 == 0:
+			print("Today is Saturday and the future day is Sunday")
+	
+			
