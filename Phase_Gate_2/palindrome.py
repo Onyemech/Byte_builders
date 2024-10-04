8c6150a233d8415c12d0bd948598210774eeb693
Onyemechi Caleb <Onyemechicaleb4@gmail.com>
2024-10-04 12:03:33 +0100

    Time of submission 12

---
+++ Phase_Gate_2/palindrome.py
@@ -0,0 +1,21 @@
+def palindrome ():
+
+	numbers = 121
+	unit = 10
+	hundred = 100
+
+	last_number = numbers // hundred
+	extract_remaining = numbers  % hundred
+	middle_number = extract_remaining  //  unit
+	first_number = extract_remaining %  unit
+	
+
+	reversed_num = (first_number * hundred) + (middle_number * unit) + last_number 
+	if reversed_num == numbers:
+		return True
+	elif reversed_num != numbers:
+		return False
+
+	print("New reversed is ",reversed_num)
+	
+palindrome ()
