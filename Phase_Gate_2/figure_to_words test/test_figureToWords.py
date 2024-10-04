c5760e519e4dfd05a09b926721da9f13933570ba
Onyemechi Caleb <Onyemechicaleb4@gmail.com>
2024-10-04 16:17:31 +0100

    Testing

---
+++ Phase_Gate_2/figure_to_words test/test_figureToWords.py
@@ -0,0 +1,8 @@
+import unittest
+import figureToWords
+
+class TestFigureToWords(unittest.TestCase)
+	def test_that_figureToWords_function_exist(self)
+		figureToWords.number_to_words("7", "5")
+
+	
