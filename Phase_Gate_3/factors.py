c71a09435ff3e372362835840720e9f396434c44
Onyemechi Caleb <Onyemechicaleb4@gmail.com>
2024-10-10 20:20:31 +0100

    i have been getting fatal message even after initializing

--- Phase_Gate_3/factors.py
+++
@@ -1,9 +0,0 @@
-#user_input = int(input("Enter your number : "))
-
-#def check_factorial (user_input):
-
-user_input = 4
-
-for number in range(user_input, 1, -1):
-	result = user_input * user_input - 1 
-print(result)
--- Phase_Gate_3/right_angle.py
+++
@@ -1,7 +0,0 @@
-for numbers in range(1, 6, 1):
-
-	for number in range(1, numbers):
-		if numbers % 2 == 0:
-			print("*",end = " ")
-	print("")
-
