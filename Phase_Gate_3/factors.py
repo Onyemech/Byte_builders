672a13bbfadcd845bd90e199ee8b1b12e4db2e03
Onyemechi Caleb <Onyemechicaleb4@gmail.com>
2024-10-10 17:16:10 +0100

    Phase Gate 3

---
+++ Phase_Gate_3/factors.py
@@ -0,0 +1,9 @@
+#user_input = int(input("Enter your number : "))
+
+#def check_factorial (user_input):
+
+user_input = 4
+
+for number in range(user_input, 1, -1):
+	result = user_input * user_input - 1 
+print(result)
---
+++ Phase_Gate_3/right_angle.py
@@ -0,0 +1,7 @@
+for numbers in range(1, 6, 1):
+
+	for number in range(1, numbers):
+		if numbers % 2 == 0:
+			print("*",end = " ")
+	print("")
+
